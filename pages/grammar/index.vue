01cbeda1b1485a6cdfead91bdbb5ef1e825a4f3d
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-05-29 11:26:54 +0500

    commit 3

--- pages/grammar/index.vue
+++ pages/grammar/index.vue
@@ -1,19 +1,47 @@
 <template>
     <main>
         <div class="container mx-auto flex justify-center text-center">
-            <div class=" border mt-[60px] w-[1280px] bg-white">
+            <div class=" border mt-[60px] w-[1280px] bg-white rounded-[18px] h-[706px]">
                 <h1 class="mt-[40px] text-[24px] font-bold h-[56px]">TABLE OF CONTENTS</h1>
                 <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px] flex justify-between">
-                    <div class="flex">
-                        <div class="bg-[#01756C] w-[50px] h-[50px] rounded-full">01</div>
-                        <p class="text-[18px]">Introduction</p>
+                    <div class="flex items-center ml-[32px]">
+                        <div class="flex items-center justify-center text-white bg-[#01756C] w-[50px] h-[50px] rounded-full">01</div>
+                        <p class="text-[18px] ml-[16px]">Introduction</p>
                     </div>
-                    <div></div>
+                    <div class="mr-[32px] mt-[24px] w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center"><img src="../../public/frame.png" alt=""></div>
                 </div>
-                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px]"></div>
-                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px]"></div>
-                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px]"></div>
-                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px]"></div>
+               
+                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px] flex justify-between">
+                    <div class="flex items-center ml-[32px]">
+                        <div class="flex items-center justify-center text-white bg-[#01756C] w-[50px] h-[50px] rounded-full">02</div>
+                        <p class="text-[18px] ml-[16px]">Present Simple</p>
+                    </div>
+                    <div class="mr-[32px] mt-[24px] w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center"><img src="../../public/frame.png" alt=""></div>
+                </div>
+                
+                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px] flex justify-between">
+                    <div class="flex items-center ml-[32px]">
+                        <div class="flex items-center justify-center text-white bg-[#01756C] w-[50px] h-[50px] rounded-full">02</div>
+                        <p class="text-[18px] ml-[16px]">Present Continues</p>
+                    </div>
+                    <div class="mr-[32px] mt-[24px] w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center"><img src="../../public/frame.png" alt=""></div>
+                </div>                
+                
+                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px] flex justify-between">
+                    <div class="flex items-center ml-[32px]">
+                        <div class="flex items-center justify-center text-white bg-[#01756C] w-[50px] h-[50px] rounded-full">02</div>
+                        <p class="text-[18px] ml-[16px]">Past Simple</p>
+                    </div>
+                    <div class="mr-[32px] mt-[24px] w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center"><img src="../../public/frame.png" alt=""></div>
+                </div>       
+
+                <div class="rounded-[32px] ml-[80px] dv w-[1120px] h-[98px] mt-[16px] flex justify-between">
+                    <div class="flex items-center ml-[32px]">
+                        <div class="flex items-center justify-center text-white bg-[#01756C] w-[50px] h-[50px] rounded-full">02</div>
+                        <p class="text-[18px] ml-[16px]">Infinity, Gerund</p>
+                    </div>
+                    <div class="mr-[32px] mt-[24px] w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center"><img src="../../public/frame.png" alt=""></div>
+                </div>            
             </div>
         </div>
     </main>
