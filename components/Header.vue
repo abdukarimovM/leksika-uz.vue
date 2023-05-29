bd766e947f6fb3a0a02bcc9d08f0190375c63111
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-05-29 14:15:56 +0500

    commit 4

--- components/Header.vue
+++ components/Header.vue
@@ -2,13 +2,13 @@
     <header class="bg-[#01756C]">
         <div class="container mx-auto">
             <nav class="h-[100px] flex justify-between items-center text-white">
-                <NuxtLink to="">
+                <NuxtLink to="/">
                     <img width="254px" height="60px" src="../public/Logo.png" alt="">
                 </NuxtLink>
                 <div class="flex gap-[80px] text-[18px]">
                     <h1>ENG <i class="bx bx-transfer-alt"></i> UZB</h1>
                     <h1>RUS <i class="bx bx-transfer-alt"></i> UZB</h1>
-                    <h1>Articles</h1>
+                    <h1><NuxtLink to="/articles">Articles</NuxtLink></h1>
                     <h1><NuxtLink to="/grammar">Grammar</NuxtLink></h1>
                     <h1><NuxtLink to="/about">About us</NuxtLink></h1>
                 </div>
---
+++ pages/articles/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <main>
+        <div class="container mx-auto ">
+            <div class="flex">
+                <div class="flex items-center bg-white w-[738px] h-[64px] rounded-[18px] mt-[60px]">
+                    <i class='bx bx-search text-[24px] text-[#AAAAAA] ml-[26px]'></i>
+                    <input class="ml-[13px] w-[550px] p-2" type="text" placeholder="Search articles here...">            
+                </div>
+                <h1 class="ml-[20px] mt-[88px] text-[24px] font-bold">Latest articles</h1>
+            </div>
+
+            <div class="flex">
+                <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                    <img class="m-[20px]" src="../../public/book.png" alt="">
+                    <div class="mt-[20px]">
+                        <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                            <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                            <p class="mr-[15px]">1 month ago</p>
+                            <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                            <p>number of reads:  52</p>
+                        </div>
+                        <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                        <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                    </div>
+                </div>
+
+                <div class="mt-[24px] w-[522px] bg-white rounded-[18px] ml-[20px]">
+                    <ul class="list-disc ml-[40px] mt-[20px] text-[#AAAAAA]">
+                        <li>Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</li>
+                        <li class="text-lg mt-[16px]">Ingliz tilini samarali o'rganish uchun 7 ta maslahat</li>
+                        <li class="mt-[16px]">O‘zbek tilining asosiy imlo qoidalari</li>
+                        <li class="mt-[16px]">Upwork'da ish boshlash</li>
+                        <li class="mt-[16px]">Most Confused Words in English</li>
+                        <li class="mt-[16px]">Most Confused Words in English</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>    
