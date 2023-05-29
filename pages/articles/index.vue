b28fe02714faf248d263948d9c33e5ae5c75f716
Abukarimov <abdukarimovmuslimbek2004@gmail.com>
2023-05-30 01:23:49 +0500

    articles end

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <div class="container mx-auto ">
+        <div class="container mx-auto">
             <div class="flex">
                 <div class="flex items-center bg-white w-[738px] h-[64px] rounded-[18px] mt-[60px]">
                     <i class='bx bx-search text-[24px] text-[#AAAAAA] ml-[26px]'></i>
@@ -10,29 +10,117 @@
             </div>
 
             <div class="flex">
-                <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
-                    <img class="m-[20px]" src="../../public/book.png" alt="">
-                    <div class="mt-[20px]">
-                        <div class="flex gap-[7px] items-center text-[#8C8B8B]">
-                            <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
-                            <p class="mr-[15px]">1 month ago</p>
-                            <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
-                            <p>number of reads:  52</p>
+                <div>
+                    <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                        <img class="m-[20px]" src="../../public/book.png" alt="">
+                        <div class="mt-[20px]">
+                            <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                                <p class="mr-[15px]">1 month ago</p>
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                                <p>number of reads:  52</p>
+                            </div>
+                            <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                            <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
                         </div>
-                        <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
-                        <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                    </div>
+                    <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                        <img class="m-[20px]" src="../../public/book.png" alt="">
+                        <div class="mt-[20px]">
+                            <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                                <p class="mr-[15px]">1 month ago</p>
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                                <p>number of reads:  52</p>
+                            </div>
+                            <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                            <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                        </div>
+                    </div>
+                    <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                        <img class="m-[20px]" src="../../public/book.png" alt="">
+                        <div class="mt-[20px]">
+                            <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                                <p class="mr-[15px]">1 month ago</p>
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                                <p>number of reads:  52</p>
+                            </div>
+                            <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                            <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                        </div>
+                    </div>
+                    <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                        <img class="m-[20px]" src="../../public/book.png" alt="">
+                        <div class="mt-[20px]">
+                            <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                                <p class="mr-[15px]">1 month ago</p>
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                                <p>number of reads:  52</p>
+                            </div>
+                            <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                            <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                        </div>
+                    </div>
+                    <div class="bg-white mt-[20px] w-[738px] rounded-[18px] flex ">
+                        <img class="m-[20px]" src="../../public/book.png" alt="">
+                        <div class="mt-[20px]">
+                            <div class="flex gap-[7px] items-center text-[#8C8B8B]">
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_cal.png" alt="">
+                                <p class="mr-[15px]">1 month ago</p>
+                                <img class="w-[17px] h-[17px]" src="../../public/icon_book.png" alt="">
+                                <p>number of reads:  52</p>
+                            </div>
+                            <h1 class="mt-[16px] text-[24px] font-bold">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</h1>
+                            <p class="w-[411px] mt-[16px]">Chet tilini o’rganishda so’z boyligini doimiy ravishda boyitib borish, ya’ni ko’proq yangi so’zlarni yod olish, juda muhimdir. Biroq, hamma ham bu ishni uddalay olmaydi. So’z boyligini oshirish ...</p>
+                        </div>
+                    </div>
+
+                    <div class="flex bg-white h-[80px] mt-[20px] mb-[80px] rounded-full shadow-2xl items-center justify-center">
+                        <div class="flex text-[#8C8B8B] items-center gap-[10px]">
+                            <i class='text-xl bx bx-left-arrow-alt' ></i>
+                            <p>PREV</p>
+                        </div>
+                        <div class="ml-[50px] flex gap-[12px]">
+                            <div class="flex items-center justify-center text-white w-[40px] h-[40px] rounded-full boreder bg-[#01756C]"><p>1</p></div>
+                            <div class="border flex items-center justify-center w-[40px] h-[40px] rounded-full boreder "><p>2</p></div>
+                            <div class="border flex items-center justify-center w-[40px] h-[40px] rounded-full boreder "><p>3</p></div>
+                        </div>
+                        <div class="ml-[50px] flex text-[#8C8B8B] items-center">
+                            <p>NEXT</p>
+                            <i class='bx bx-right-arrow-alt'></i>
+                        </div>
                     </div>
                 </div>
 
-                <div class="mt-[24px] w-[522px] bg-white rounded-[18px] ml-[20px]">
-                    <ul class="list-disc ml-[40px] mt-[20px] text-[#AAAAAA]">
-                        <li>Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</li>
-                        <li class="text-lg mt-[16px]">Ingliz tilini samarali o'rganish uchun 7 ta maslahat</li>
-                        <li class="mt-[16px]">O‘zbek tilining asosiy imlo qoidalari</li>
-                        <li class="mt-[16px]">Upwork'da ish boshlash</li>
-                        <li class="mt-[16px]">Most Confused Words in English</li>
-                        <li class="mt-[16px]">Most Confused Words in English</li>
-                    </ul>
+                <div class="">
+                    <div class="mt-[24px] w-[522px] bg-white rounded-[18px] ml-[20px] h-[264px]">
+                        <ul class="list-disc ml-[40px] text-[#AAAAAA] text-[16px]">
+                            <li class="pt-[20px]">Ingliz tilidagi so'zlarni eslab qolishing 11 ta samarali usuli</li>
+                            <li class="mt-[16px]">Ingliz tilini samarali o'rganish uchun 7 ta maslahat</li>
+                            <li class="mt-[16px]">O‘zbek tilining asosiy imlo qoidalari</li>
+                            <li class="mt-[16px]">Upwork'da ish boshlash</li>
+                            <li class="mt-[16px]">Most Confused Words in English</li>
+                            <li class="mt-[16px]">Most Confused Words in English</li>
+                        </ul>
+                    </div>
+                    <div class="ml-[20px] mt-[30px]">
+                        <h1 class="text-[24px] font-bold">Tags</h1>
+                        <div class="text-[#AAAAAA] flex text-[16px] mt-[20px] gap-[16px]">
+                            <div class="flex items-center justify-center rounded-full bg-white w-[124px] h-[56px]">English (2)</div>
+                            <div class="flex items-center justify-center rounded-full bg-white w-[154px] h-[56px]">Freelancer (5)</div>
+                            <div class="flex items-center justify-center rounded-full bg-white w-[162px] h-[56px]">Vocabulary (5)</div>
+                        </div>
+                        <div class="text-[#AAAAAA] flex text-[16px] mt-[16px] gap-[16px]">
+                            <div class="flex items-center justify-center rounded-full bg-white w-[197px] h-[56px]">Uzbek grammar (2)</div>
+                            <div class="flex items-center justify-center rounded-full bg-white w-[207px] h-[56px]">English grammar (5)</div>
+                        </div>
+                        <div class="text-[#AAAAAA] flex text-[16px] mt-[16px] gap-[16px]">
+                            <div class="flex items-center justify-center rounded-full bg-white w-[103px] h-[56px]">Imlo (2)</div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
